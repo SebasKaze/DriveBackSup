fc97be17dbd78eaed972c627cf8083268ab2c83b
Your Name <you@example.com>
2026-02-16 19:38:36 -0600

    otro commit

--- src/index.js
+++ src/index.js
@@ -7,14 +7,33 @@ import empresaRoutes from "./routes/empresa.js";
 
 const PORT = process.env.PORT || 4000;
 
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://drive-front-ced3yhihl-sebaskazes-projects.vercel.app"
+];
+
+
 const app = express();
 
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://drive-front-one.vercel.app/"
-    ]
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true); // permitir Postman/curl
+
+        if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+        } else {
+        callback(new Error("No permitido por CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+// 👇 IMPORTANTE: manejar preflight explícitamente
+app.options("*", cors());
+
+
 app.use(express.json());
 
 app.use("/empresa", empresaRoutes);
