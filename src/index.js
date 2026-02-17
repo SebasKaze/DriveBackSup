036a836e4e24301bad6e9187ea51a1bd3c96ace6
Your Name <you@example.com>
2026-02-16 20:47:44 -0600

    otro mas as

--- src/index.js
+++ src/index.js
@@ -17,19 +17,26 @@ const allowedOrigins = [
 const app = express();
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
 
-        if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-        } else {
-        return callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    // Permitir localhost
+    if (origin.startsWith("http://localhost")) {
+      return callback(null, true);
+    }
+
+    // Permitir cualquier subdominio vercel.app
+    if (origin.endsWith(".vercel.app")) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 app.use(express.json());
 
